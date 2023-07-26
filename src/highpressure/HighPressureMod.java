8fc58ab939222329dfbf752837a5f8d2cdc163b7
Idonotus <reubinius@gmail.com>
2023-07-26 19:13:07 +0100

    lil content

--- src/highpressure/HighPressureMod.java
+++ src/highpressure/HighPressureMod.java
@@ -1,17 +1,15 @@
-package example;
+package highpressure;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import highpressure.content.HPBlocks;
 
-public class ExampleJavaMod extends Mod{
+public class HighPressureMod extends Mod{
 
-    public ExampleJavaMod(){
+    public HighPressureMod(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
@@ -21,7 +19,7 @@ public class ExampleJavaMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("high-pressure-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
@@ -30,6 +28,7 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
+        HPBlocks.load();
         Log.info("Loading some example content.");
     }
 
---
+++ src/highpressure/content/HPBlocks.java
@@ -0,0 +1,41 @@
+package highpressure.content;
+
+import mindustry.content.Items;
+import mindustry.type.Category;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.distribution.Conveyor;
+import mindustry.world.blocks.environment.StaticWall;
+import mindustry.world.blocks.environment.Floor;
+import mindustry.world.Block;
+import mindustry.world.meta.Attribute;
+import highpressure.world.blocks.power.*;
+import static mindustry.type.ItemStack.*;
+
+public class HPBlocks {
+    public static Block siliconConveyor,hardiceFloor,hardiceWall,laserenergizer;
+
+    public static void load(){
+        siliconConveyor = new Conveyor("silicon-conveyor"){{
+            requirements(Category.distribution, with(Items.silicon, 10, Items.graphite, 4));
+            speed = 0.055f;
+            size = 1;
+            displayedSpeed = 8f;
+            health = 60;
+        }};
+        hardiceFloor = new Floor("hard-ice"){{
+            variants = 8;
+            dragMultiplier = 0.01f;
+            albedo = 0.3f;
+            speedMultiplier = 0.8f;
+            attributes.set(Attribute.water,-0.4f);
+        }};
+        hardiceWall = new StaticWall("hard-ice-wall"){{
+           variants = 4;
+        }};
+        laserenergizer = new LaserEnergizer("laser-energizer"){{
+            requirements(Category.power,with(Items.surgeAlloy,90));
+            size = 2;
+        }};
+    }
+}
